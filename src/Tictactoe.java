d36d9b6794fda09879b7cd361aa4e957e2e6d53f
Finnholio <finnholio@bigby.org>
2020-09-17 15:13:09 -0500

    Made Tic tac toe game.

--- src/Tictactoe.java
+++ src/Tictactoe.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Tictactoe {
     public static void main(String args[]){
         //rules
-        System.out.println("How to play tic tac toe in this program : X's are represented by -1 and O's are represented by 1.\nX's go first and to input which square you want to place your piece on you first input your row then your column.");
+        System.out.println("");
 
-        Scanner myObj = new Scanner(System.in);
         boolean playingGame = true;
         int whosTurn = -1;
         int[][] game = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
@@ -24,31 +23,100 @@ public class Tictactoe {
                 printGameState(game);
 
                 //get input
-                System.out.println("Row : ");
-                String row = myObj.next();
-                System.out.println("Column : ");
-                String column = myObj.next();
+                if(check_winner(game) == -1){
+                    System.out.println("Player 1's Turn : ");
+                }
+                else if(check_winner(game) == 1) {
+                    System.out.println("Player 2's Turn : ");
+                }
 
-                int row_int = Integer.parseInt(row);
-                int column_int = Integer.parseInt(column);
+                //place new piece
+                boolean no_piece_placed = true;
+                while(no_piece_placed){
+                    int[] newPiecePosition = getInput();
+                    if(game[newPiecePosition[0]][newPiecePosition[1]] == 0){
+                        game[newPiecePosition[0]][newPiecePosition[1]] = whosTurn;
+                        no_piece_placed = false;
+                    }
+                    else {
+                        System.out.println("ERROR : PIECE PLACEMENT  INVALID");
+                        System.out.println("\n\n");
+                    }
 
-                game[row_int][column_int] = whosTurn;
+                }
 
+                //switch turn
                 if (whosTurn == -1){
                     whosTurn = 1;
                 }
                 else {
                     whosTurn = -1;
                 }
+
+                //space between lines
+                System.out.println("\n\n");
             }
         }
 
     }
 
+    public static int[] getInput(){
+        Scanner inputScanner = new Scanner(System.in);
+        char inputText[] = inputScanner.next().toCharArray();
+
+        int outputPosition[] = {0, 0};
+
+        //define row
+        outputPosition[0] = (inputText[1] - 49);
+
+        //define column
+        if(inputText[0] == 't'){
+            outputPosition[1] = 0;
+        }
+        else if(inputText[0] == 'm'){
+            outputPosition[1] = 1;
+        }
+        else if(inputText[0] == 'b'){
+            outputPosition[1] = 2;
+        }
+        else {
+            System.out.println("ERROR : CHARACTER NOT RECOGNIZED");
+        }
+
+        return outputPosition;
+    }
+
     public static void printGameState(int[][] game){
-        System.out.println(game[0][0] + "|" + game[1][0] + "|" + game[2][0]);
-        System.out.println(game[0][1] + "|" + game[1][1] + "|" + game[1][2]);
-        System.out.println(game[2][0] + "|" + game[2][1] + "|" + game[2][2]);
+        //make game readable
+        String[][] gameRead = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(game[i][j] == -1){
+                    gameRead[i][j] = "X";
+                }
+                else if(game[i][j] == 1){
+                    gameRead[i][j] = "O";
+                }
+                else {
+                    if (j == 0){
+                        gameRead[i][j] = "t" + (i+1);
+                    }
+                    else if (j == 1){
+                        gameRead[i][j] = "m" + (i+1);
+                    }
+                    else if (j == 2){
+                        gameRead[i][j] = "b" + (i+1);
+                    }
+                    else {
+                        System.out.println("ERROR : GAME HAS GONE OUT OF BOUNDS");
+                    }
+                }
+            }
+        }
+
+        System.out.println(gameRead[0][0] + "|" + gameRead[1][0] + "|" + gameRead[2][0]);
+        System.out.println(gameRead[0][1] + "|" + gameRead[1][1] + "|" + gameRead[2][1]);
+        System.out.println(gameRead[0][2] + "|" + gameRead[1][2] + "|" + gameRead[2][2]);
     }
 
     public static int check_winner(int[][] game){
